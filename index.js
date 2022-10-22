905d9b9d6b07760dadb5f718c4d48f1e84fa192a
NazarSavchuk <nazarsavcuk3@gmail.com>
2022-10-22 15:54:29 +0200

    auth init

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import mongoose from "mongoose";
 import multer from "multer";
+import cors from "cors";
 
 import {
   registerValidation,
@@ -32,6 +33,7 @@ const storage = multer.diskStorage({
 const upload = multer({ storage });
 
 app.use(express.json());
+app.use(cors());
 app.use("/uploads", express.static("uploads"));
 
 app.post(
@@ -55,6 +57,8 @@ app.post("/upload", checkAuth, upload.single("image"), (req, res) => {
 });
 
 app.get("/posts", PostController.getAll);
+app.get("/tags", PostController.getLastTags);
+
 app.get("/posts/:id", PostController.getOne);
 
 app.post(
