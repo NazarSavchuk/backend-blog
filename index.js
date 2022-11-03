fb3a409b53adfba920ed78fafb94fee05312ea33
NazarSavchuk <nazarsavcuk3@gmail.com>
2022-11-03 23:26:05 +0100

    deploy fix

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import express from "express";
 import mongoose from "mongoose";
 import multer from "multer";
 import cors from "cors";
-
+//fix it
 import {
   registerValidation,
   loginValidation,
